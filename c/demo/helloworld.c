3573eafb9bfb4c88a034c1234f9af3849d685450
what-can-be-name <xbjwuyu@163.com>
2016-10-20 09:05:55 +0800

    Update helloworld.c

--- c/demo/helloworld.c
+++ c/demo/helloworld.c
@@ -1,5 +1,6 @@
 #include <stdio.h> /*引用标准输入输出库*/
-int main（）{
-  printf("Hello World\n");
-  return 0;
+int main（）        //定义主函数
+{
+  printf("Hello World\n");    //输出
+  return 0;         //返回零值
 }
