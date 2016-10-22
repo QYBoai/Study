995f4f517578bdfe8790ce30260ee7beef517efa
郭俊余 <hagbgreen@yahoo.com>
2016-10-23 01:58:33 +0800

    注释风格调整(//->/**/),缩进风格调整(space->tab)

--- c/demo/helloworld.c
+++ c/demo/helloworld.c
@@ -1,6 +1,6 @@
-#include <stdio.h> /*引用标准输入输出库*/
-int main（）        //定义主函数
+#include <stdio.h>	/*引用标准输入输出库*/
+int main（)	/*定义主函数*/
 {
-  printf("Hello World\n");    //输出
-  return 0;         //返回零值
+  printf("Hello World\n");	/*输出 Hello World */
+  return 0;	/*返回0*/
 }
